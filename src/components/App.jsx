4aa67be3fccc54c7d96cccbd6079766cacdc0e58
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-03-16 01:26:18 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -40,3 +40,59 @@
 // }
 
 // export default App;
+
+
+import React, { useState } from "react";
+
+function App() {
+  // creating object of useState to avoid writing multiple states and fns
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+  function handleChange(event) {
+    // const newValue=event.target.value;
+    // // store korchi user input take
+    // const inputName=event.target.name;
+    const { value, name } = event.target;
+    // prevValue te previous content ta store hoye jacche
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form>
+        <input
+          name="fName"
+          onChange={handleChange}
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          onChange={handleChange}
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
